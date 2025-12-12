f4652d7866e48eec93fadb1817b2ced692a16f7d
mayf <m13160102112@163.com>
2025-12-12 13:25:43 +0800

    更换websocket地址

--- packages/majsoul-api/src/config/config.js
+++ packages/majsoul-api/src/config/config.js
@@ -1,8 +1,8 @@
 // default config
 module.exports = {
-    workers: 1,
-    majsoulBaseUrl: 'https://game.maj-soul.com/1/',
-    majsoulWssUrl: 'wss://gateway-hw.maj-soul.com/gateway',
-    majsoulUserName: '',
-    majsoulPassword: ''
+  workers: 1,
+  majsoulBaseUrl: 'https://game.maj-soul.com/1/',
+  majsoulWssUrl: 'wss://route-2.maj-soul.com/gateway',
+  majsoulUserName: '',
+  majsoulPassword: ''
 };
